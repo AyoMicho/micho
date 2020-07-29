1b86b0cbefe13f756db64c49f991626818b2bcdc
AyoMicho <michoayomide@gmail.com>
2020-07-29 23:29:11 +0100

    I deleted everything in all files

--- script.js
+++
@@ -1,8 +0,0 @@
-// navigation nav bar
-
-const ham = document.getElementById("ham");
-const nav = document.getElementById("nav");
-
-ham.addEventListener("click", () => {
-    nav.classList.toggle("show");
-})
